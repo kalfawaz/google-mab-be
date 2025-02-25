411706e67fc11dca9cf8a1c9812953c8306f1164
kalfawaz@tahakom.com <kalfawaz@tahakom.com>
2025-02-25 12:52:28 +0300

    fisrt backend setup

---
+++ src/main/java/com/project/google_map_be/GoogleMapBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.google_map_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/google_map_be/GoogleMapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.google_map_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
